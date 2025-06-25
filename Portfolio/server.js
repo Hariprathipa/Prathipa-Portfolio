7b02a5ab9749531de9c64e8145290321efe7fb7d
Hariprathipa <hariprathipa.r,ece.2023@snsct.org.com>
2025-06-25 06:29:58 +0530

    fix template string for console.log

--- Portfolio/server.js
+++ Portfolio/server.js
@@ -52,5 +52,5 @@ app.post('/contact', async (req, res) => {
 // ✅ 5. Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(🚀 Server running on port ${PORT});
+  console.log(`🚀 Server running on port ${PORT}`);
 });
